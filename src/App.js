e1da90bfc6de4f0c5c9ad36a995b71a734110c3b
Reed James <reedtjames1234@gmail.com>
2019-07-24 18:41:11 -0500

    Push for new pull

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   //     features: []
   //   },
   //   store: [
-  //     { id: 1, name: 'V-6 engine', price: 1500 },
+  //     { id: 1, name: 'V-6 engine', price: 1600 },
   //     { id: 2, name: 'Racing detail package', price: 1500 },
   //     { id: 3, name: 'Premium sound system', price: 500 },
   //     { id: 4, name: 'Rear spoiler', price: 250 }
