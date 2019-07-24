7af61867f69148e2f16eebb5eaaa7b143118186e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-07-24 16:24:07 -0700

    Wrap components with redux provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+const AppWithProvider = (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+ReactDOM.render(AppWithProvider, rootElement);
