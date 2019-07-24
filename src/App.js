23ebbb85850453637323eb3b63b13151e8663053
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-07-24 15:31:53 -0500

    Bryce Soghigian

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { createStore } from 'redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+
+const store = createStore();
+
+
 const App = () => {
+
+
   const state = {
     additionalPrice: 0,
     car: {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider><App /></Provider>, rootElement);
