fa3f2b03ca0ae0b65ce0d35a90622f251e4e5019
Reed James <reedtjames1234@gmail.com>
2019-07-24 15:49:24 -0500

    Reducer.js created with initial state default reducer

---
+++ src/store/reducers.js
@@ -0,0 +1,15 @@
+export const initialState = {
+  carParts: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 }
+  ]
+};
+
+export const carReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
