1fefeb0dcc8d4634a7ba388f981fdb4df940daa6
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-08-14 16:29:17 -0500

    add button added to additional feature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const AdditionalFeature = props => {
+
+  // const buyItem = item => {
+  //   // dipsatch an action here to add an item
+  // };
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={props.addButton(props.feature)} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
