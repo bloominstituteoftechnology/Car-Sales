d5fd646e416b59a4486deac70390501b5a716a84
dangolbeeker <scottb305@outlook.com>
2019-08-14 17:43:44 -0400

    actions and reducer done

---
+++ src/actions/carActions.js
@@ -0,0 +1,10 @@
+export const takeAwayFeature = item => {
+    return { type: 'TAKEAWAY_FEATURE'}
+};
+takeAwayFeature();
+
+export const addItem = item => {
+    return { type: 'ADD_ITEM'};
+
+}
+addItem();
