147b570361dee1c94087d3eab36ca6f665ab93ba
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-08-14 16:40:53 -0500

    additionalfeatures.js updated

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import AdditionalFeature from './AdditionalFeature';
+import {addingFeatures} from '../actions'
 
 const AdditionalFeatures = props => {
+
+  const buyItem = item => {
+    props.addingFeatures(item)
+    // dipsatch an action here to add an item
+  };
+
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.store.length ? (
         <ol type="1">
           {props.store.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} addButton={buyItem}/>
           ))}
         </ol>
       ) : (
@@ -18,4 +26,13 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return {
+    store: state.store,
+  }
+}
+
+export default connect(
+  mapStateToProps, 
+  { addingFeatures}
+  )(AdditionalFeatures);
