13e8e2955f8b5e40adeac3f8769018361f592549
johnschneider1 <johnlawschneider@gmail.com>
2019-08-14 17:57:08 -0500

    add and delete

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const AddedFeature = props => {
+  console.log("remove test:", props);
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        onClick={() => props.removeItem(props.feature)}
+        className="button"
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import AddedFeature from './AddedFeature';
+import AddedFeature from "./AddedFeature";
 
 const AddedFeatures = props => {
+  console.log("features test:", props);
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+            <AddedFeature
+              key={item.id}
+              feature={item}
+              removeItem={props.removeItem}
+            />
           ))}
         </ol>
       ) : (
