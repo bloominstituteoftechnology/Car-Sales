7a6cf56f2592500f5a30bcb62577fc83985084da
seongck <seongk@pm.me>
2019-09-24 21:44:47 -0700

    Connect Total component to Redux store

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${props.price + props.additionalPrice}</h4>
     </div>
   );
 };
 
-export default Total;
+const mapStateToProps = state => {
+  return {
+    price: state.priceReducer.car.price,
+    additionalPrice: state.priceReducer.additionalPrice
+  };
+};
+
+export default connect(mapStateToProps)(Total);
