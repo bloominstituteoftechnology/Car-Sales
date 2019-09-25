38b19a1f6a6d9df5fe63b19b01483cda80a13079
seongck <seongk@pm.me>
2019-09-24 22:48:03 -0700

    Connect AdditionalFeature component and implement onClick actions

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+
+import { addFeature } from "../actions";
+import { updateAdditionalPrice } from "../actions";
 
 const AdditionalFeature = props => {
+  const addFeature = e => {
+    e.preventDefault();
+    props.addFeature(props.feature);
+    props.updateAdditionalPrice();
+  };
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={addFeature} className="button">
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(
+  null,
+  { addFeature, updateAdditionalPrice }
+)(AdditionalFeature);
