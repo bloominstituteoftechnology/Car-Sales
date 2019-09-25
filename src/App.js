316c6ccd09c0a42cdbc853e2bc1225ea80b398ea
seongck <seongk@pm.me>
2019-09-24 21:45:37 -0700

    Remove passed in state prop on Total component

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
       </div>
       <div className="box">
         <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <Total />
       </div>
     </div>
   );
