e062c72de31d8444af865deeaa561e9a52a6517a
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-09 14:45:25 +0100

    Remove functions from App.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ import Total from './components/Total';
 import * as actions from './state/actionCreators';
 
 const App = ({ buyItem, removeFeature, vehicle }) => {
-  debugger
-
-  // const removeFeature = item => {
-  //   // dispatch an action here to remove an item
-  // };
-
-  // const buyItem = item => {
-  //   // dipsatch an action here to add an item
-  // };
-
   return (
     <div className="boxes">
       <div className="box">
