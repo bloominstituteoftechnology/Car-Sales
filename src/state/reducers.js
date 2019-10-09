aa0ba80c19a9a7afbe843d9a809bdf6dcdc5d677
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-09 14:55:15 +0100

    Add button fully working

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -26,19 +26,21 @@ const initialState = {
 export function vehicleReducer(state = initialState, action) {
   switch (action.type) {
     case types.BUY_ITEM:
-      debugger
       return {
           ...state,
+          additionalPrice: state.additionalPrice + action.payload.item.price,
           car: {
               ...state.car,
               features: state.car.features.concat(action.payload.item)
           },
+          store: state.store.filter(item => item.id !== action.payload.item.id),
       };
     case types.REMOVE_FEATURE:
       return {
           ...state,
           car: {
-              features: state.car.features.filter(feature => feature.id !== action.payload.id)
+            ...state.car,
+            features: state.car.features.filter(feature => feature.id !== action.payload.id)
           }
       };
     default:
