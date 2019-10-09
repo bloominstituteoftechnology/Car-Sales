39806b07ee281870dbb60fa24c56e1693788107c
Dustin Myers <dustinmyers@users.noreply.github.com>
2019-10-09 09:10:48 -0600

    Change confusing store name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
         'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
       features: []
     },
-    store: [
+    additionalFeatures: [
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
