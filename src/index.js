6de45f40029eb084953b365d8784a6e8a1110727
Jeremy Rogel <jeremyrogel@Jeremys-MBP.lan1>
2019-10-09 18:44:30 -0400

    finished mvp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux'
+import { reducer } from './reducers'
+import { Provider } from 'react-redux'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, 
+    rootElement);
