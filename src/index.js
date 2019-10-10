11300c167cbf37b6410a5493f6110c59793d8fd7
kiyani <bamba.kiyani@gmail.com>
2019-10-09 23:05:25 -0400

    Inject the store into our Provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import * as reducers from './state/reducers';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
@@ -11,7 +12,13 @@ const rootReducer = combineReducers({
     state: reducers.carReducer
 });
 
+// Step 5 - nourrish our management state (store) our reducers functions
+const store = createStore(rootReducer);
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-<App />, 
+    // Step 6 - inject the store into our Provider, and make all our slices available to all components within our app  
+    <Provider store={store}>
+        <App />
+    </Provider>, 
 rootElement);
