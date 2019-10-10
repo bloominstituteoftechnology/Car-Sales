ea8879b589cd56297437a65ee19db3f3014d2c8a
Dustin Myers <dustinmyers@users.noreply.github.com>
2019-10-10 09:33:02 -0600

    Update AdditionalFeatures.js

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -5,9 +5,9 @@ const AdditionalFeatures = props => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.store.length ? (
+      {props.additionalFeatures.length ? (
         <ol type="1">
-          {props.store.map(item => (
+          {props.additionalFeatures.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
