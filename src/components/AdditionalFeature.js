ac57db142c444cd23d885801fcdae55f4df2e051
kiyani <bamba.kiyani@gmail.com>
2019-10-10 00:16:04 -0400

    Repeated same step for AdditionalFeatures component

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/actionCreator';
 
-const AdditionalFeature = props => {
+export const AdditionalFeature = ({feature, addFeature}) => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      <button onClick={() => addFeature(feature)} className="button">Add</button>
+      {feature.name} (+{feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(
+  state => state, 
+  actionCreators,
+)
+(AdditionalFeature);
