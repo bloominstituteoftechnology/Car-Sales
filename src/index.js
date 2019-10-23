aaa418b461f75803399924e947452e0250d37d35
Carlos <miklo.concepcion@gmail.com>
2019-10-22 19:35:01 -0500

    onClicks added to AdditionalFeature and AddedFeature functional components.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+// importing the createStore function from redux
+import { createStore } from 'redux';
+// importing the Provider from react-redux so I can wrapp my <Aoo /> in it. Nice and cozy
+import { Provider } from "react-redux";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+function reducer() {
+  return {
+    // title: 'Hello world! I\'m in the Redux store!',
+  }
+}
+
+const store = createStore(reducer);
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
