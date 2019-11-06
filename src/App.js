3b46b956cc8ec11872dc8ee69ba79a8013342cb9
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-06 18:53:20 -0500

    set up store and provider

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   const buyItem = item => {
     // dipsatch an action here to add an item
   };
-
+  
   return (
     <div className="boxes">
       <div className="box">
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_FEATURE, DELETE_FEATURE } from "./actions";
+// import { ADD_FEATURE, DELETE_FEATURE } from "./actions";
 
 export const initialState = {
     additionalPrice: 0,
