fa1a43748c9f4544737e2529acc4b7983ee952f4
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-11-06 16:17:47 -0600

    added features to all components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { reducer } from './reducers/reducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
