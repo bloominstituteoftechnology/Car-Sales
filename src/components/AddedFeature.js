be4882791c52f2dc24a0b177bc45a6283dad0bf7
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-07 12:19:23 -0500

    removes items

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { connect } from "react-redux";
-import { addFeature } from "../actions/actions";
+import { connect } from 'react-redux';
+import { removeFeature } from '../actions/actions.js';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={() => {props.addFeature(props.feature)}}>Add</button>
-      {props.feature.name} (+{props.feature.price})
+      <button className="button" onClick={()=>{props.removeFeature(props.feature)}}>Remove</button>
+      {props.feature.name}(+{props.feature.price})
     </li>
   );
 };
 
 const mapStateToProps = state => {
+  console.log('mapstatetoprops: ', state);
   return {
     additionalPrice: state.additionalPrice,
     car: state.car,
-    additionalFeatures: state.additionalFeatures
+    additionalFeatures: state.additionalFeatures,
   }
 }
 
-export default connect(mapStateToProps, {addFeature})(AddedFeature);
+export default connect(mapStateToProps, {removeFeature})(AddedFeature);
