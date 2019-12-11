c25dd353a488e62459aea7e5e2316af8ffaaada8
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-12-11 13:54:03 -0800

    user can add and remove features to a car in a checkout process, met MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {removeFeature} from '../actions/carActions';
 
 const AddedFeature = props => {
+
+  const removeFeature = () => {
+    console.log('remove feature: ' + props.feature.name);
+    props.removeFeature(props.feature);
+  }
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={removeFeature}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, {removeFeature})(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {addFeature} from '../actions/carActions';
 
 const AdditionalFeature = props => {
+
+  const addFeature = () => {
+    console.log('add feature: ' + props.feature.name)
+    props.addFeature(props.feature);
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={addFeature}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, {addFeature})(AdditionalFeature);
