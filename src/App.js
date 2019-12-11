a2f5e14f323c906c5cb7424f1b8eca7fe3214008
Aasa-Christian <aasachristian@yahoo.com>
2019-12-11 17:25:53 -0500

    used redux to connect state from reducer to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+const App = (props) => {
   
 
   const removeFeature = item => {
@@ -19,15 +20,25 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return{
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice
+  }
+}
+
+export default connect(
+  mapStateToProps, {}
+)(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -15,7 +15,7 @@ const initialState = {
     ]
   };
 
-  export const reducer = (state = initialState, action){
+  export const reducer = (state = initialState, action) => {
 
     switch (action.type){
 
