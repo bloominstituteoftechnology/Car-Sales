72f3ed53115e4bd8f75879369f92a1cbc9931dc5
JorPoon <jormpoon@gmail.com>
2019-12-11 17:19:59 -0500

    installed redux thunk/logger and applied them  to src/index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 //redux
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 
 //style
 import 'bulma/css/bulma.css';
@@ -13,7 +15,10 @@ import './styles.scss';
 //reducer
 import {carReducer} from "./reducers"
 
-const store = createStore(carReducer);
+const store = createStore(
+    carReducer,
+    applyMiddleware(thunk, logger)
+    );
 
 
 const rootElement = document.getElementById('root');
