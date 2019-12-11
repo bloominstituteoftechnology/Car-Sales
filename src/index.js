616f7f9f0bd9a97dfcdc96685746e6bfd62e3d42
JorPoon <jormpoon@gmail.com>
2019-12-11 17:03:11 -0500

    set up src index.js file with redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+//style
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore();
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, 
+    rootElement);
