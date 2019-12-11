5418cedc0bf82e80cc8cc4a162787d44168a1dbf
amichalo421 <andre.michalowski@gmail.com>
2019-12-11 16:21:55 +0100

    Car features reducer

---
+++ src/redux/carFeaturesReducer.js
@@ -0,0 +1,28 @@
+const INITIAL_STATE = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
+  
+  const useReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case 'PURCHASE_ITEM':
+        return {
+          ...state,
+          car: action.paylod
+        }
+      default:
+        return state;
+    }
+  }
