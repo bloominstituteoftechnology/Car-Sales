c308a765570bbc90bc884678121a508acaf20a5a
JorPoon <jormpoon@gmail.com>
2019-12-11 17:38:01 -0500

    setting up actions

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+//actions for add features
+
+export const ADD_FEATURES = "ADD_FEATURES"
+
+export const addFeatures = id = dispatch => {
+    dispatch({type: ADD_FEATURES, payload: id})
+}
+
+//actions for remove feature
+
+export const REMOVE_FEATURES = "REMOVE_FEATURES"
+
+export const removeFeatures = id = dispatch => {
+    dispatch({type: REMOVE_FEATURES, payload: id})
+}
