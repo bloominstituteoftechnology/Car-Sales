b84980c2c0b025813ea43402c51e7bdc81a55a62
taterntots <matthew.a.weidner@gmail.com>
2019-12-11 15:07:35 -0500

    built action creators for removeFeature and buyItem

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = (props) => {
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
-
   return (
     <div className="boxes">
       <div className="box">
