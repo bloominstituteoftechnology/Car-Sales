033d422d06d43794f511ca5308d4e21bbfcbffe1
taterntots <matthew.a.weidner@gmail.com>
2019-12-11 15:03:40 -0500

    connected my components using mapStateProps in my app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
+import { connect } from 'react-redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+const App = (props) => {
   const removeFeature = item => {
     // dispatch an action here to remove an item
   };
@@ -17,15 +18,23 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  }
+}
+
+export default connect(mapStateToProps, { }) (App);
