8fe7c60a87732340075e0db29542a6595835d4c9
JorPoon <jormpoon@gmail.com>
2019-12-12 19:56:39 -0500

    added price update actions in action/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,23 @@
 
 export const ADD_FEATURES = "ADD_FEATURES"
 
-export const addFeatures = (id) => dispatch => {
-    dispatch({type: ADD_FEATURES, payload: id})
+export const addFeatures = featureID => dispatch => {
+    dispatch({type: ADD_FEATURES, payload: featureID})
 }
 
 //actions for remove feature
 
 export const REMOVE_FEATURES = "REMOVE_FEATURES"
 
-export const removeFeatures = (id) => dispatch => {
-    dispatch({type: REMOVE_FEATURES, payload: id})
+export const removeFeatures = featureID => dispatch => {
+    dispatch({type: REMOVE_FEATURES, payload: featureID})
+}
+
+
+//actions for price total
+
+export const UPDATE_TOTAL_PRICE = "UPDATE_TOTAL_PRICE"
+
+export const updatePrice = featureID => dispatch => {
+    dispatch({type: UPDATE_TOTAL_PRICE, payload: featureID})
 }
