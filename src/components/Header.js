bbc642e8d28263a16f2d855ee622996eed203cbb
CLYPR Technologies <cbanks@clypr.co>
2020-01-08 20:36:46 -0500

    mvp complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = props => {
   return (
     <>
-      <figure className="image is-128x128">
+      <figure className="image">
         <img src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
