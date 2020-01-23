fc525575fc7e93a85f4aca412aeb162361c1e0a9
wesley.moody <wgm3190@gmail.com>
2020-01-22 18:11:06 -0600

    doneski

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
         <img src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
+      <p>Amount: ${props.car.price + props.additionalPrice}</p>
     </>
   );
 };
