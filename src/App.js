8d2f81659cdec527fdf14cbe2b22c955215ee8b9
Jeff Breig <jjbreig@gmail.com>
2020-02-04 23:28:37 -0500

    fixed project dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
