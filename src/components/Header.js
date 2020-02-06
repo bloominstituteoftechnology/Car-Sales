091b84acdcd230e8c776c18b7f7263dd6f7ec74c
Jeff Breig <jjbreig@gmail.com>
2020-02-06 17:09:38 -0500

    MVPgit add . git add .

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,29 @@ import { Provider, connect } from "react-redux";
 import { buyItem, removeItem } from "../actions/index";
 
 const Header = props => {
+
+let featurePrices = 0;
+let featurePricesTotal=0;
+const sumPrices =(totalPrice, price) =>{
+  return totalPrice+price;
+}
+
+  if (props.car.features.length){
+    featurePrices = props.car.features.map(feature=>feature.price);
+    console.log(featurePrices);
+    featurePricesTotal=featurePrices.reduce(sumPrices, 0);
+  }
+  else{
+    featurePrices=0
+  }
+console.log("Feature Price: ", featurePricesTotal)
   return (
     <>
       <figure className="image is-128x128">
         <img src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
+      <p>Amount: ${props.car.price + featurePricesTotal}</p>
     </>
   );
 };
