116956c3cdb6f16dacdb346d2751a7edf69614a0
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-02-06 19:25:44 -0800

    Completed MVP

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import { connect } from "react-redux";
 
 const AdditionalFeatures = props => {
+
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature 
+            key={item.id} 
+            feature={item} 
+            buyItem={props.buyItem}
+            />
           ))}
         </ol>
       ) : (
--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers/reducer.js';
+
+ const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store= {store}>
+        <App />
+    </Provider>, rootElement);
