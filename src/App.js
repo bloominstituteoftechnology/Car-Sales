bab59047eaeaddb8c64e2d5445e057154b85517f
Veto Ramirez <vetorobert3@gmail.com>
2020-02-06 17:28:01 -0800

    Created store and an empty reducer.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { createStore } from 'redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-
-const store = createStore(reducer); 
 
 const App = () => {
   const state = {
