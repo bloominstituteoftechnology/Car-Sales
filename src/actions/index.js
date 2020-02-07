2e879026736ae601a104a5ca27fd2e175bcf813c
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-02-06 17:34:07 -0800

    messing w/ index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,37 +6,48 @@ const REMOVE_FEATURE_PRICE = "REMOVE_FEATURE_PRICE";
 
 const UPDATE_TOTAL = "UPDATE_TOTAL";
 
-export const addFeature = feature => {
+const addFeature = feature => {
     return{
         type: ADD_FEATURE,
         payload: feature
     }
 }
 
-export const removeFeature = feature => {
+const removeFeature = feature => {
     return{
         type: REMOVE_FEATURE,
         payload: feature
     }
 }
 
-export const addFeaturePrice = featurePrice => {
+const addFeaturePrice = featurePrice => {
     return{
         type: ADD_FEATURE_PRICE,
         payload: featurePrice
     }
 }
 
-export const removeFeaturePrice = featurePrice => {
+const removeFeaturePrice = featurePrice => {
     return{
         type: REMOVE_FEATURE_PRICE,
         payload: featurePrice
     };
 };
 
-export const updateTotal = total => {
+const updateTotal = total => {
     return {
         type: UPDATE_TOTAL,
         payload: total
     }
 }
+
+export {
+    ADD_FEATURE,
+    addFeature,
+    REMOVE_FEATURE,
+    removeFeature,
+    ADD_FEATURE_PRICE,
+    addFeaturePrice,
+    REMOVE_FEATURE_PRICE,
+    removeFeaturePrice
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStpre } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import allReducers from "./reducers";
 
