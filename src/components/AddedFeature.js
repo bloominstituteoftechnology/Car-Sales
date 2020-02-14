ba361a642d9b465887e2c69d0c539bc27c94eb45
AustinKelsay <austinkelsay@yahoo.com>
2020-02-14 09:02:09 -0600

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {removeFeature} from "../state/Actions";
+import { connect } from 'react-redux';
 
 const AddedFeature = props => {
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={() => props.removeFeature(props.feature)} className="button">Remove Feature</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, {removeFeature})(AddedFeature);
