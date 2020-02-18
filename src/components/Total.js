910af3893b40ecdf4b2533d52119cea11fe5abec
Alec Dye <alecdye@gmail.com>
2020-02-18 15:20:36 -0800

    configuring initialState

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Total = props => {
+  console.log("Total component", props)
   return (
     <div className="content">
       <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
@@ -8,4 +10,11 @@ const Total = props => {
   );
 };
 
-export default Total;
+//* update price from additionalPrice when feature is added
+const mapStateToProps = state => {
+  return {
+    price: state.price
+  }
+}
+
+export default connect(mapStateToProps, {})(Total);
