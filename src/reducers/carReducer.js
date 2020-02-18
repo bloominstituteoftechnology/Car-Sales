951db410519958b2490fed9e9472fb11284bf49d
alexis villaraza <alexis.borja@gmail.com>
2020-02-18 13:28:36 -0700

    added initialState to carReducer

---
+++ src/reducers/carReducer.js
@@ -0,0 +1,16 @@
+export const initialState = {
+    additionalPrice: 0,
+    car: {
+        price: 26395,
+        name: '2019 Ford Mustang',
+        image:
+            'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+        features: []
+    },
+    additionalFeatures: [
+        { id: 1, name: 'V-6 engine', price: 1500 },
+        { id: 2, name: 'Racing detail package', price: 1500 },
+        { id: 3, name: 'Premium sound system', price: 500 },
+        { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+};
