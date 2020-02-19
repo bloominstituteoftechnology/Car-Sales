0e01181ea3e064b22012435878d6acd1b3cc5530
Mandi Haase <haase1020@gmail.com>
2020-02-19 03:08:06 -0500

    add const removeFeatureHandler to AddedFeatures.js along with const mapStateToProps

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { removeFeature, removeFeaturePrice } from '../actions/actions.js';
+import { connect } from 'react-redux';
+
 
 import AddedFeature from './AddedFeature';
 
 const AddedFeatures = props => {
+
+  const removeFeatureHandler = feature => {
+    props.removeFeature(feature);
+    props.removeFeaturePrice(feature.price)
+  };
+
+
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+            <AddedFeature key={item.id} feature={item} removeFeatureHandler={removeFeatureHandler} />
           ))}
         </ol>
       ) : (
@@ -19,4 +29,9 @@ const AddedFeatures = props => {
   );
 };
 
+const mapStateToProps = state => {
+  return {
+    car: state.carReducer
+  }
+}
 export default AddedFeatures;
