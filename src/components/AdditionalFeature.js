18ac2a566c3a0db07d645dc1bbf6827cc8619565
Mandi Haase <haase1020@gmail.com>
2020-02-19 04:43:11 -0500

    input information to actions.js

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { addFeature, addFeaturePrice } from '../actions/actions';
+import { addFeature, addFeaturePrice } from "../actions/actions";
 import { connect } from 'react-redux';
 
+
 const AdditionalFeature = props => {
-
-const featureHandler = feature => {
-  feature.preventDefault();
-  props.addFeature(props.feature);
-  props.addFeaturePrice(props.feature.price);
-}
-
-
-
+  
+  const featureHandler = feature => {
+    feature.preventDefault();
+    props.addFeature(props.feature);
+    props.addFeaturePrice(props.feature.price);
+  }
+  
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button" onClick={featureHandler}>Add</button>
+      <button className="button" onClick={featureHandler} >Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -10,9 +10,9 @@ const AdditionalFeatures = props => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+      {props.store.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {props.store.map(item => (
             <AdditionalFeature key={item.id} feature={item} featureHandler={props.featureHandler} />
           ))}
         </ol>
