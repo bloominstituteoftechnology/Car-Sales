1be7d98726451a5192c61f79da78f4bf596986ce
Mandi Haase <haase1020@gmail.com>
2020-02-19 02:52:46 -0500

    add to index.js file to import Provider and createStore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import Reducers from './reducers/reducers';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+const store = createStore(Reducers);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
