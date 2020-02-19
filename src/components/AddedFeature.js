950af11335854a70cfe4becd297c3ed8190e47c5
Mandi Haase <haase1020@gmail.com>
2020-02-19 03:00:05 -0500

    add onClick removeFeatureHandler to AddedFeature.js

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const AddedFeature = props => {
+  const removeFeatureHandler = () => {
+    props.removeFeatureHandler(props.feature)
+  }
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={removeFeatureHandler}>X</button>
       {props.feature.name}
     </li>
   );
