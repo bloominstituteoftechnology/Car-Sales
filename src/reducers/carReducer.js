61a2e98372bcefa82e37b84e75aa7ba8badabe70
Mandi Haase <haase1020@gmail.com>
2020-02-19 03:37:37 -0500

    fill out carReducer file

--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -1,5 +1,30 @@
+import { ADD_FEATURE, REMOVE_FEATURE } from '../actions/actions';
+import { statement } from '@babel/template';
+
+const initialState = {
+    price: 50000,
+    name: "2020 Tesla Model S",
+    image: 'https://www.teslarati.com/wp-content/uploads/2019/07/tesla-model-s-raven-2.jpg',
+    features:[]
+};
 
 
 
+const carReducer = (staet = initialState, action) => {
+    switch(action.type){
+        case ADD_FEATURE:
+            return {
+                ...state,
+                features: [...state.features, action.payload]
+            };
+        case REMOVE_FEATURE:
+            return {
+                ...state,
+                features: state.features.filter(feature => feature.id !== action.payload.id)
+            }
+            default:
+                return state;
+    }
+}
 
-image: 'https://www.teslarati.com/wp-content/uploads/2019/07/tesla-model-s-raven-2.jpg',
+export carReducer;
