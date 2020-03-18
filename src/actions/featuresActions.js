f9f723b3c855e4cad15cf9724e84ac20014f007f
Nikki Lin <nikkiylin@gmail.com>
2020-03-17 23:20:56 -0700

    done

---
+++ src/actions/featuresActions.js
@@ -0,0 +1,11 @@
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+export const ADD_FEATURE = 'ADD_FEATURE';
+
+export const removeFeature = (feature) => {
+    return { type: REMOVE_FEATURE, payload: feature };
+}
+
+export const addFeature = (feature) => {
+    return { type: ADD_FEATURE, payload: feature };
+}
+
