ad5e6a493fd9bf44354a36dfad72b2a70164bc66
aaronmatson1 <matsonaaron1@gmail.com>
2020-04-12 22:16:23 -0600

    MVP Done.

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -4,7 +4,7 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={() => props.removeFeature(props.feature.name)}>X {props.feature.name}</button>
+      <button className="button" onClick={() => props.removeFeature(props.feature)}>X {props.feature.name}</button>
     </li>
   );
 };
--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -1,4 +1,4 @@
-import {REMOVE_FEATURE} from "../actions";
+import {REMOVE_FEATURE, BUY_ITEM} from "../actions";
 
 const initialState = {
   additionalPrice: 0,
@@ -6,7 +6,7 @@ const initialState = {
     price: 26395,
     name: '2019 Ford Mustang',
     image:'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-    features: [{ id: 1, name: 'V-6 engine', price: 1500 }]
+    features: []
   },
   additionalFeatures: [
     { id: 1, name: 'V-6 engine', price: 1500 },
@@ -20,9 +20,13 @@ export const carReducer = (state = initialState, action) => {
     switch(action.type) {
         case REMOVE_FEATURE:
           return {
-              ...state, car: { ...state.car, features: state.car.features.filter(feature => feature.name !== action.payload) }
+            ...state, car: { ...state.car, price: state.car.price - action.payload.price, features: state.car.features.filter(feature => feature.id !== action.payload.id) }
+          }
+        case BUY_ITEM:
+          return {
+            ...state, car: { ...state.car , price: state.car.price + action.payload.price, features: [...state.car.features, action.payload]}
           }
         default:
           return state
     }
-} 
+}
