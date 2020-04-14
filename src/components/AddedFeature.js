985007dabca851913dfd1fac7795252b92201ac2
Alison Ludick <alison.ludick@gmail.com>
2020-04-14 23:28:54 +0200

    imported and implemented react-redux lib

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/actionCreators';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={e => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(state => state.app, actionCreators)(AddedFeature);
