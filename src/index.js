29101e8662eca2095dd52d9e3815073846cd314a
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-04-14 14:04:59 -0700

    maniReducer created with initial state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore()
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<App />
+, rootElement);
