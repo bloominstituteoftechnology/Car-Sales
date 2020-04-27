af5dfd47cf35f0755b2c579387b637b8e107abf6
Lauren <leladrummonds@gmail.com>
2020-04-26 21:00:20 -0400

    updates
    reducer.js & index.js updates

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux'; // import Provider to make the store available to the app components wrapped in the connect function
+import { createStore } from 'redux'; // import to create a store object for holding state data
+import { reducer } from './reducers/reducer'; // import reducer to be the argument passed into createStore
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer); // this line creates a store that will hold the state data
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}> {/* Pass store to Provider as props so components can access it */}
+        <App />
+    </Provider>,
+    rootElement
+);
+
