7a0c852f82fb26b20769c3fc9e732656c05326af
Lauren <leladrummonds@gmail.com>
2020-04-26 21:49:27 -0400

    updates
    more updates

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux'; // import to link the AddedFeature component with the store
+import { removeFeature } from '../actions/actions'; // imported removeFeature action creator
 
 const AddedFeature = props => {
-  return (
-    <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
-    </li>
-  );
+    return (
+        <li>
+            {/* Add an onClick to run a function to remove a feature */}
+            <button className="button"
+                    onClick={()=>
+                    {props.removeFeature(props.feature)}}>
+                X
+            </button>
+            {props.feature.name} (+{props.feature.price})
+        </li>
+    );
 };
 
-export default AddedFeature;
+// mapStateToProps takes state data for features so its properties can be accessed via props in this component
+const mapStateToProps = state => {
+    return {
+        features: state.features // Now the state data for all the feature object's properties can be accessed via props
+    }
+}
+
+// The connect function links the AddedFeature component with the store for retrieving data it needs from the store
+// connect dispatches the removeFeature action action creator to the reducer and allows removeFeature to be accessed via props
+export default connect(mapStateToProps, { removeFeature }) (AddedFeature);
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux'; // import to link the Total component with the store
 
 const Total = props => {
-  return (
-    <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
-    </div>
-  );
+    return (
+        <div className="content">
+            <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+        </div>
+    );
 };
 
-export default Total;
+// mapStateToProps takes state data price and additionalPrice and returns data that can be used as props in this component
+const mapStateToProps = state => {
+    return {
+        car: state.car, // Now the state data for all car properties can be accessed via props in this component
+        additionalPrice: state.additionalPrice // Now the state data for additionalPrice can be accessed via props in this component
+    }
+}
+
+//  The connect function links the Total component with the store for retrieving data it needs from the store
+export default connect(mapStateToProps, {})(Total);
