adf5d696c564b46444d2908ffd3a978244988c94
Christina Giddens <giddens.christina@gmail.com>
2020-04-28 14:01:59 -0400

    Met MVP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './reducers/reducer';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
