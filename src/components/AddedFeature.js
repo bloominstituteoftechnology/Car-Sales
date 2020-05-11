8106b0f2120c810c72d041ef6e9db9403ad2ad46
Sandra Coburn <sandra_coburn@hotmail.com>
2020-05-11 16:50:42 -0700

    Added remove features functionality

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { removeFeature } from "../actions/feauturesActions";
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        className="button"
+        onClick={() => props.removeFeature(props.feature)}
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
+const mapStatetoProps = (state) => {
+  return {
+    car: state.car,
+  };
+};
 
-export default AddedFeature;
+export default connect(mapStatetoProps, { removeFeature })(AddedFeature);
