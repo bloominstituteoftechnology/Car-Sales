92df92bdb48b4fbf57b700b9504f32e8f566d072
Dustin Myers <dustinmyers@users.noreply.github.com>
2020-05-12 09:15:07 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,14 +23,6 @@ const App = () => {
     ]
   };
 
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
-
   return (
     <div className="boxes">
       <div className="box">
