de1d58aafd9652c61f2c2e71a8848048774b2da7
Ryan Erickson <rerickson21@yahoo.com>
2020-06-19 20:16:49 -0400

    MVP

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {addFeature} from '../actions/rootActions' 
+import { connect } from 'react-redux';
 
 const AdditionalFeature = props => {
+  console.log(props)
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={() => props.addFeature(props.feature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = state => {
+  //   console.log(state.additionalFeatures);
+  //   return {
+  //     feature: state.additionalFeatures.feature, 
+  //     price: state.additionalFeatures.price, 
+  //   };
+  
+  // };
+  
+  export default connect(null,{addFeature})(AdditionalFeature);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {createStore} from 'react-redux'; 
+import {Provider} from 'react-redux'; 
+import {rootReducer} from './reducers/rootReducer'; 
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(rootReducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store} ><App /></Provider>, rootElement);
