568c63373b36e59604805edf564ecfc00947e64d
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-06-23 17:20:50 -0400

    starting over, reducer/actions/store done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { appReducer } from './reducers';
+
+// create store
+const store = createStore(appReducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+ rootElement);
