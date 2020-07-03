afc9f55ac2beb272b56b169eb9d5aad6c1b22e4c
Nick Perez <nick@Nicks-MacBook-Pro.local>
2020-07-03 13:57:54 -0700

    fix mobile view

--- src/reducers/featuresReducer.js
+++ src/reducers/featuresReducer.js
@@ -15,7 +15,10 @@ export const initialState = {
     { id: 1, name: "V-6 engine", price: 1500 },
     { id: 2, name: "Racing detail package", price: 1500 },
     { id: 3, name: "Premium sound system", price: 500 },
-    { id: 4, name: "Rear spoiler", price: 250 }
+    { id: 4, name: "Rear spoiler", price: 250 },
+    { id: 5, name: "Performance Exhaust", price: 800 },
+    { id: 6, name: "Intake Upgrade", price: 500 },
+    { id: 7, name: "Chrome Rims", price: 2000 }
   ]
 };
 
