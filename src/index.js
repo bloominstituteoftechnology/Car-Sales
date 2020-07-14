3bed7da2b5c3485687734fc1354c05161c1cb57b
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-07-14 12:41:23 -0700

    add reducer with initial state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { carReducer } from './reducers/carReducer';
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
