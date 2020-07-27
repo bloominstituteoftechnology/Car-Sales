594daaeb9e94ca8e10ce62ca8ffc1db585fe68cc
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-07-26 21:16:02 -0400

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Header = props => {
   return (
     <>
-      <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
+      <figure className="image is-256x256">
+        <img className= "is-rounded"  src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
+      <p>From: ${props.car.price} +</p>
     </>
   );
 };
