4032319b3d734cac1f2465a43d0aef2dd40594a2
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-11 18:57:01 -0400

    feature addition components should be functional

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 import AddedFeature from './AddedFeature';
 
@@ -9,7 +10,7 @@ const AddedFeatures = props => {
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+            <AddedFeature key={item.id} feature={item} removeItem={props.removeItem} />
           ))}
         </ol>
       ) : (
@@ -19,4 +20,10 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+function mapStateToProps(state) {
+  return {
+   state: state
+  }
+}
+
+export default connect(mapStateToProps)(AddedFeatures);
