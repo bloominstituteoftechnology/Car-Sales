445b2f364f64b3308cc4321cc27e2522023edbcd
Cora Krynick <krynickcora@gmail.com>
2020-08-12 17:19:41 -0500

    first push

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { reducer } from './reducer/reducer'
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
+
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
