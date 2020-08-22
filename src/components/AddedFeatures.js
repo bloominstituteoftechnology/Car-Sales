abd917050a2f33e6ac307e867ce9c2a2c4b552f7
dipeshkoirala <dipeshkoirala@gmail.com>
2020-08-21 22:11:52 -0400

    ...

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -6,9 +6,9 @@ const AddedFeatures = (props) => {
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {props.state.car.features.length ? (
         <ol type="1">
-          {props.car.features.map((item) => (
+          {props.state.car.features.map((item) => (
             <AddedFeature
               key={item.id}
               feature={item}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { theReducer } from "./reducers/theReducer";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-
+import { logger } from "redux-logger";
 import "bulma/css/bulma.css";
 import "./styles.scss";
 
-const store = createStore(theReducer);
+const store = createStore(theReducer, applyMiddleware(logger));
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
