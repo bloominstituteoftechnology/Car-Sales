5a61d7618252e309261decf96200afd2cb278a04
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-24 18:51:02 -0500

    completed assignment, met MVP requirements

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { removeFeature } from '../actions/actions';
+import { connect } from 'react-redux';
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        className="button"
+        onClick={() => props.removeFeature(props.feature.id)}
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapDispatchToProps = {
+  removeFeature,
+}
+
+export default connect(null, mapDispatchToProps)(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addFeature } from '../actions/actions';
 
-const AdditionalFeature = props => {
+const AdditionalFeature = (props) => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button
+        className="button"
+        onClick={() => props.addFeature(props.feature)}
+      >
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapDispatchToProps = { addFeature };
+
+export default connect(null, mapDispatchToProps)(AdditionalFeature);
