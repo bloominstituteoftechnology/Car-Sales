abac8548329dd8d406550fe442a37e83dd04abcc
Luke Adams <muhammadadams@gmail.com>
2020-08-25 22:08:37 -0400

    mvp

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+/****action types***/
+
+export const ADD_FEATURE = "ADD_FEATURE";
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+
+/***action creators***/
+
+export const addFeature = (data) => {
+  return {
+    type: ADD_FEATURE,
+    payload: data,
+  };
+};
+
+export const removeFeature = (data) => {
+  return {
+    type: REMOVE_FEATURE,
+    payload: data,
+  };
+};
--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import AddedFeature from './AddedFeature';
+import AddedFeature from "./AddedFeature";
 
-const AddedFeatures = props => {
+const AddedFeatures = (props) => {
+  //console.log(props.item);
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
+          {props.car.features.map((item) => (
             <AddedFeature key={item.id} feature={item} />
           ))}
         </ol>
@@ -19,4 +21,11 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+const mapStateToProps = (state) => {
+  //console.log(state);
+  return {
+    car: state.car,
+  };
+};
+
+export default connect(mapStateToProps, {})(AddedFeatures);
