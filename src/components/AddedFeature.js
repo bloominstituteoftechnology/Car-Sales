acd75c805ee57377458e697daa81a415134a9f6e
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-09-10 14:42:13 -0700

    fixed deleted feature

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -7,7 +7,7 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={()=> props.removeFeature(props.feature)}>X</button>
+      <button className="button" onClick={()=> props.deleteFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
