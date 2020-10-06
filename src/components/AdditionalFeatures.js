5ce04e7bc238b2dab148bcdcec1fa6b327083dd1
unknown <tompsherman@gmail.com>
2020-10-06 17:09:44 -0400

    initial commit
    moved state to store, set up Provider

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import AdditionalFeature from './AdditionalFeature';
+import React from "react";
+import AdditionalFeature from "./AdditionalFeature";
 
-const AdditionalFeatures = props => {
+const AdditionalFeatures = (props) => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {props.additionalFeatures.map((item) => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
