a09f233fcca625531bfdbf583e70d01d0309f495
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-06 20:06:06 -0600

    state finally moved into reducer

--- src/reducers/featureReducer.js
+++ src/reducers/featureReducer.js
@@ -1,8 +1,20 @@
 import { ADD_NEW_FEATURE, REMOVE_FEATURE } from "../actions/featureActions";
 
 const initialState = {
-    features: [],
-    header: "Features"
+        additionalPrice: 0,
+        car: {
+          price: 26395,
+          name: '2019 Ford Mustang',
+          image:
+            'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+          features: []
+        },
+        additionalFeatures: [
+          { id: 1, name: 'V-6 engine', price: 1500 },
+          { id: 2, name: 'Racing detail package', price: 1500 },
+          { id: 3, name: 'Premium sound system', price: 500 },
+          { id: 4, name: 'Rear spoiler', price: 250 }
+        ]
 };
 
 function newFeature(name) {
@@ -25,4 +37,4 @@ export const featureReducer = (state = initialState, action) => {
         }
     };
 
-    
+
