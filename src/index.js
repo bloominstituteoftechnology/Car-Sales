2735601aae1194ee643e4272ab60e9c0135a0da3
AdamMaley <adamemaley@yahoo.com>
2020-10-18 21:03:55 -0400

    pretty tough to figure out but MVP, I need to look it over to understand better.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux"
+import { createStore } from "redux";
+import { featureReducer } from "./reducers/reducer";
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(featureReducer);
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, rootElement);
