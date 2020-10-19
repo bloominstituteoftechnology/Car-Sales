a797c7690a6fb6b876f35b1505fc1b9ae518768f
Mason <masondavis2009@gmail.com>
2020-10-19 18:46:57 -0500

    Mvp complete

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -4,7 +4,7 @@ const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={() => props.buyItem(props.feature)} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
