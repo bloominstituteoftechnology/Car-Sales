9650c1ac9bdd6f61705c01bc0537a1989fb3df19
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-10-22 22:49:13 -0400

    store created and reducer is set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+
+
 
 const App = () => {
   const state = {
