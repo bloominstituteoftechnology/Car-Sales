927afa2215f34eb6cc9ab1fd1e873b918d0d3c1d
jayharville <jayharville@aol.com>
2020-10-25 13:10:09 -0700

    Completed MVP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducer from "./reducers";
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+);
