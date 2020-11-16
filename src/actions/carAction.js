6b89a0a434d5c5224af0bdecc2f24e389c13b538
Ryan Clark <rbclark244@gmail.com>
2020-11-15 22:31:58 -0500

    MVP

---
+++ src/actions/carAction.js
@@ -0,0 +1,16 @@
+export const ADD_NEW_FEATURE = "ADD_NEW_FEATURE";
+export const REMOVE_NEW_FEATURE = "REMOVE_NEW_FEATURE";
+
+export const addNewFeature = (newFeature) => {
+  return {
+    type: ADD_NEW_FEATURE,
+    payload: newFeature,
+  };
+};
+
+export const removeNewFeature = (feature) => {
+  return {
+    type: REMOVE_NEW_FEATURE,
+    payload: feature,
+  };
+};
