7f3f6e0c221a2911e2aa860d250317a8f92d0f91
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-11-18 19:27:29 -0700

    able to add features now but total has a bug

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,5 @@
-export const ADD_FEATURE = 'ADD_FEATURE'
-export const REMOVE_FEATURE = 'REMOVE_FEATURE'
+export const ADD_FEATURE = 'ADD_FEATURE';
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
 
 
 export const addFeature = (newFeature) => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './App';
+import { featureReducer } from './reducers/featureReducer'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(featureReducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
