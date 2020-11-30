d902bdd7adffabd5c15f5c8989ae80c87d0fbfe9
Aaron <54789688+flowersa29@users.noreply.github.com>
2020-11-30 16:27:55 -0600

    added connect, action, and button to additionFeature component
    added actioncreator and button to additionalFeatures

--- src/Reducers/additionalFeaturesReducer.js
+++ src/Reducers/additionalFeaturesReducer.js
@@ -1,15 +1,24 @@
-const initialState ={ additionalFeatures:[
-  { id: 1, name: 'V-6 engine', price: 1500 },
-  { id: 2, name: 'Racing detail package', price: 1500 },
-  { id: 3, name: 'Premium sound system', price: 500 },
-  { id: 4, name: 'Rear spoiler', price: 250 }
-]
+import { ADD_NEW_FEATURE } from "../Actions/additionalNewFeaturesAction"
 
+const initialState = {
+  additionalFeatures: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 },
+  ],
+  header: "test",
 }
 
 export const additionalFeaturesReducer = (state = initialState, action) => {
-    switch(action.type){
-      default:
-        return state
-    }
+  console.log("Redux is calling Reducer with: ", state, action)
+  switch (action.type) {
+    case "ADD_NEW_FEATURE":
+      return {
+        ...state,
+        additionalFeatures: [...state.additionalFeatures, { name: action.payload, price: action.payload }],
+      }
+    default:
+      return state
+  }
 }
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -6,6 +6,7 @@ const AdditionalFeatures = props => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
+      
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
