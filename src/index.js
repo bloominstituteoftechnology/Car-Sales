df04e3dd12461a91834a9a9f15eadd67acb08995
cmirza <mail@cmirza.com>
2020-12-08 19:56:56 -0800

    MVP complete except layout is wonky after removing items

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { reducer } from './reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={ store }><App /></Provider>, rootElement);
