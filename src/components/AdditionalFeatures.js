8b23a43942ed5d09309a60beffee2d3020cc90c2
Tony So <tonyso@Tonys-MacBook-Pro.local>
2020-12-23 16:15:17 -0600

    completed functionality with reducer (not redux)

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -8,7 +8,7 @@ const AdditionalFeatures = props => {
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature buyItem={props.buyItem} key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
