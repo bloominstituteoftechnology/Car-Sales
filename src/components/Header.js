d1c106bef11fb75d7011087f81a891b6732fd2bf
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2021-01-19 17:04:04 -0500

    de bugging

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-const Header = props => {
+const Header = (props) => {
   return (
     <>
       <figure className="image is-128x128">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { connect } from "react-redux";
+import { Provider } from "react-redux";
+import { reducer } from "./Reducers";
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+const store = createStore(reducer);
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
