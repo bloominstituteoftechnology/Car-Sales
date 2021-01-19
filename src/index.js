5092ef4edf7692c4e3498210f69feed7b248e6aa
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-01-19 16:15:13 -0600

    successfully connected state to props for app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import reducer from "./Redux/reducer";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(reducer);
+// console.log(store.getState())
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
