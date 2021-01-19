7e341dac011cea83d5c6b64165ea9700aa725bba
Jean Luciano <jeanluciano@mailbox.org>
2021-01-19 17:52:49 -0600

    with hooks instead of connect

--- src/App.js
+++ src/App.js
@@ -4,30 +4,25 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-import {connect} from "react-redux"
+import {useSelector} from "react-redux"
 const App = (props) => {
   
-
+  const car = useSelector(state => state.car)
+  const additionalFeatures = useSelector(state=> state.additionalFeatures)
+  const additionalPrice = useSelector(state => state.additionalPrice)
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <Header car={car} />
+        <AddedFeatures car={car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
-        <Total car={props.car} additionalPrice={props.additionalPrice} />
+        <AdditionalFeatures/>
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
   );
 };
-const mapStateToProps = (state) => {
-  return {
-    car: state.car,
-    additionalFeatures: state.additionalFeatures,
-    additionalPrice:state.additionalPrice
-  }
-}
 
-export default connect(mapStateToProps,{}) (App);
+export default App
 
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import AdditionalFeature from './AdditionalFeature';
+import React from "react";
+import AdditionalFeature from "./AdditionalFeature";
+import {useSelector} from "react-redux"
 
-const AdditionalFeatures = props => {
-  console.log(props)
+const AdditionalFeatures = (props) => {
+  console.log(props);
+  const additionalFeatures = useSelector(state=> state.additionalFeatures)
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+      {additionalFeatures.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {additionalFeatures.map((item) => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
