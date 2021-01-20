04ab99c9beb4086ea25676f77933c628654cdd58
Beau Jennings <redalthazar@gmail.com>
2021-01-19 22:32:44 -0600

    find resolution for compilation error in index.js involving name on disk

--- src/App.js
+++ src/App.js
@@ -14,13 +14,7 @@ const App = () => {
       image:
         'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
       features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
+    }
   };
 
   return (
