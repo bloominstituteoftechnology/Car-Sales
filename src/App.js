d163c46630494f943e15a593a528b43d31fe2713
george-hatzigeorgio <georgehatzigeorgio@zohomail.com>
2021-01-20 10:31:23 -0500

    code is breaking

--- src/App.js
+++ src/App.js
@@ -4,22 +4,40 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { connect } from "react-redux"
 
-const App = () => {
+const App = (props) => {
+
+  
   
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={this.state.car} />
+        <AddedFeatures car={this.state.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={this.state.additionalFeatures} />
+        <Total car={this.state.car} additionalPrice={this.state.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+
+
+
+const mapStateToProps = state =>{
+  return  {
+    car: state.car,
+    price: state.price,
+    name:  state.name,
+    image: state.image,
+    features:state.features,
+    additionalPrice: state.additionalPrice,
+    additionalFeatures:state.additionalFeatures
+  
+}
+}
+export default connect(mapStateToProps,{},)(App)
--- src/index.js
+++ src/index.js
@@ -5,34 +5,10 @@ import { Provider } from "react-redux"
 import 'bulma/css/bulma.css';
 import './styles.scss';
 import { createStore } from "redux"
+import { reducer } from "./components/reducer/reducer"
 
 
-const reducer = () => {
-    return (
-    {
-            additionalPrice: 0,
-           car: {
-             price: 26395,
-             name: '2019 Ford Mustang',
-             image:
-               'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-             features: []
-           },
-           additionalFeatures: [
-             { id: 1, name: 'V-6 engine', price: 1500 },
-             { id: 2, name: 'Racing detail package', price: 1500 },
-             { id: 3, name: 'Premium sound system', price: 500 },
-             { id: 4, name: 'Rear spoiler', price: 250 }
-           ]
-        }
-
-
-
-
-
-    )
-}  
-    const store=createStore(reducer,-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    const store=createStore(reducer,+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 
