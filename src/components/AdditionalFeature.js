de2a426a0a535f784742eb833e4dd26b4633c250
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-01-21 20:10:11 -0700

    much better this time around

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { addFeature } from './../actions/actions'
 
 const AdditionalFeature = props => {
+
+  const handleClick = () => {
+    props.addFeature(props.feature);
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={handleClick} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, {addFeature})(AdditionalFeature);
--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import './styles.scss';
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from "./reducers/reducer"
+import reducer from './reducers/reducer'
+
 
 const store = createStore(reducer);
 
