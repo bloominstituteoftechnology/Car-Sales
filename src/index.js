6731ccb27178f38ae78d278c5d66428c353fdba9
ngwatso <ngwatson79@yahoo.com>
2021-01-31 17:14:19 -0500

    set up Provider, createStore on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+// !! Placeholder reducer
+const reducer = () => {};
+
+const store = createStore(reducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
