1d829c569aa5a1b578c38dc982d4bf1550abb577
Corie Stewart <coriestewart1989>
2021-02-16 15:52:57 -0500

    finished

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {removeFeature} from '../actions'
 
-const AddedFeature = props => {
+const AddedFeature = ({removeFeature, feature}) => {
+
+  const handleClick =e=>{
+    removeFeature(feature)
+  }
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button onClick={handleClick} className="button">X</button>
+      {feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, {removeFeature})(AddedFeature);
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Total = props => {
   return (
     <div className="content">
