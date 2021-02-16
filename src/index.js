4c7c2557ef7a8818464daa76b48b2fc95c86540c
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-02-16 16:45:04 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore } from "redux"
 import { Provider} from "react-redux"
+import reducer from "./components/reducer/index"
 
  
 import 'bulma/css/bulma.css';
@@ -11,7 +12,7 @@ import './styles.scss';
 
 const ReactDevTools  = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore( ReactDevTools)
+const store = createStore( reducer,ReactDevTools)
 
 
 const rootElement = document.getElementById('root');
