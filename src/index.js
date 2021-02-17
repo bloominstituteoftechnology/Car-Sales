2463c52a13a30b272a57ed8de171448894835bf4
hdm007 <harry.mardirossian13@gmail.com>
2021-02-17 00:25:26 -0500

    need to add connection and handlers, then test add feature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux"
+import { Provider } from "react-redux"
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import {reducer} from "./actions"
+
+const store = createStore(reducer)
+
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store} >
+        <App /> 
+    </Provider>, rootElement);
---
+++ src/reducers/index.js
@@ -0,0 +1,47 @@
+import { ADD_FEATURE, REMOVE_FEATURE } from "../actions"
+
+
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
+
+  export const reducer = (state = initialState, action) => {
+    console.log("action type:", action.type);
+    console.log("action payload:", action.payload);
+
+    switch (action.type){
+        case ADD_FEATURE:
+            return {
+                //copy state and reduce the price 
+                ...state,
+                additionalPrice: state.additionalPrice + action.payload.price,
+                car: {
+                    //copy state of car, set car.features equal to current features in copy of state, with payload feature.
+                    ...state.car,
+                    features: [state.car.features, action.payload]
+                },
+                additionalFeatures: [
+                    //return copy of state where features remaining are not features with the payload id. Successfully removes from addtl.
+                    ...state.additionalFeatures.filter(
+                        feature => feature.id !== action.payload.id
+                    )
+                ]
+            };
+        default:
+            return state
+    }
+
+  }
