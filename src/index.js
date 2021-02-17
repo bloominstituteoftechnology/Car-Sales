5ef8f5c2e7d40191aec698c7731bd2a42061672c
blackcatwizard <55123268+bigbootygoblins@users.noreply.github.com>
2021-02-16 19:36:07 -0800

    mvp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { reducer } from './Reducers/Reducer';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import 'bulma/css/bulma.css';
-import './styles.scss';
+import './styles.css';
+
+const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+     rootElement);
