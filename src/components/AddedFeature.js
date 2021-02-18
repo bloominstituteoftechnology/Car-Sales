338d8460bc6f77d251337745f30973d4f254dd09
Chaz Carbis <chazcarbis@gmail.com>
2021-02-17 21:38:44 -0600

    done

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {addFeature} from '../actions/carActions'
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick= {() => {props.addFeature(props.feature.id)}}>X</button>
       {props.feature.name}
     </li>
   );
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {addFeature} from '../actions/carActions'
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button"  onClick= {props.addFeature}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, {addFeature})(AdditionalFeature);
