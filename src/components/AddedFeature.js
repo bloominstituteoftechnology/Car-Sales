9b187b8eccc529671f8cd65b193b15f4be1b8a8d
Anhtuan Tran <anhtuantran.github@gmail.com>
2021-02-17 20:17:02 -0600

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeFeature } from '../actions/appActions'
 
 const AddedFeature = props => {
+  const {feature, removeFeature } = props;
+  const handleRemoveFeature = () => {
+    removeFeature(feature);
+  }
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button onClick={handleRemoveFeature} className="button">X</button>
+      {feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, {removeFeature})(AddedFeature);
--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers/appReducer';
+
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
