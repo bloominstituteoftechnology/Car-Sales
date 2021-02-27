1ce92794ea6ea463875442bcf72ffcbed8aa341f
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-02-27 11:32:06 -0700

    updated reducer

--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -1,22 +1,47 @@
 const initialState = {
-    addFeature: [],
-}
+    additionalPrice: 0,
+    car: {
+        price: 26395,
+        name: '2019 Ford Mustang',
+        image: 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+        features: []
+    },
+    additionalFeatures: [
+        {
+         id: 0,
+         name: 'V-6 engine',
+         price: 1500,
+        },
+        {
+         id: 1,
+         name: 'Racing detail package',
+         price: 1500,
+        },
+        {
+         id: 2,
+         name: 'Premium sound system',
+         price: 250
+        }
+    ]
+};
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case 'ADD_FEATURE':
             return {
                 ...state,
-                addFeature: [...state.addFeature, action.payload]
-            };
+                additionalFeatures: state.additionalFeatures.filter((feature) => feature.id !== action.payload.id),
+                car: { ...state.car, price: state.car.price + action.payload.price, features: [...state.car.features, action.payload] }};
+
 
             case 'REMOVE_FEATURE':
                 return {
                     ...state,
-                    addFeature: state.addFeature.filter(addFeature => addFeature.id !== addFeature.payload)
-                };
+                    car: { ...state.car, features: state.car.features.filter((feature) => feature.id !== action.payload.id),
+                    price: state.car.price - action.payload.id }, additionalFeatures: [...state.additionalFeatures, action.payload] };
+
                 default: 
-                return state;
+                    return state;
     }
 }
 
