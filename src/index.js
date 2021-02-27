3047c3c7356177e8d2627e19e7579718511ccdfc
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-02-26 20:47:15 -0700

    added store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducer/Reducer'
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, rootElement);
