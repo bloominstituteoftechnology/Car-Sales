6ea4598081c6331d49e02f20d1ddbb6cfafb1776
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-28 11:39:19 -1000

    initial commit. Set up index.js provider, store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+//1. install redux and react-redux
+//2. import wrapper and store
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//3. create store passing in rootReducer
+const store = createStore(rootReducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+//4. Wrap App in Provider passing store as props
+<Provider store={ store }>
+<App />
+</Provider>
+, rootElement);
