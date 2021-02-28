f7d2a3887b93f00d4cba3474fbd1aeda12c84029
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-28 12:24:42 -1000

    connected addedFeature to redux..

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button 
+      className="button"
+      onClick={() => props.removedFeature( props.feature.id )}      
+      >X</button>
+      { props.feature.name }
     </li>
   );
 };
+const mapStateToProps = ( state ) => {
+  return {
+    car: state.featuresReducer.car
+  }
+}
 
-export default AddedFeature;
+
+export default connect(mapStateToProps, AddedFeature);
