0d737bf9b2683ae7810da4fd0cb8497c4073f914
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-02-28 18:03:50 -0800

    Project complete

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { deleteFeature } from '../actions';
+
+
 
 const AddedFeature = props => {
+
+  const handleClick = (e) => {
+    props.deleteFeature(props.feature.id);
+  };
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button 
+      className="button"
+      onClick={handleClick}
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null,{ deleteFeature })(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { addFeature } from '../actions/index';
+import { connect } from 'react-redux';
 
 const AdditionalFeature = props => {
+
+  const handleClick = (e) => {
+    props.addFeature(props.feature.id);
+    // console.log(props.feature.id, "Feature id") 
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={handleClick}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, { addFeature })(AdditionalFeature);
