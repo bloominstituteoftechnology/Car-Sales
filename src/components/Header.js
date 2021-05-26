bf6b4c663442261a7045cdcd01d985b223d3c87d
rockyFierro <finnslighthouse@gmail.com>
2021-05-25 21:26:00 -0700

    updated package-lock

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Header = props => {
   return (
-    <>
+    <div>
       <figure className="image is-128x128">
         <img src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
       <p>Amount: ${props.car.price}</p>
-    </>
+    </div>
   );
 };
 
